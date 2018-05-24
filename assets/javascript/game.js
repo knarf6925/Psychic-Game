eddad3b5ef5e2cf60d5b17ab5640bf4cb50c1923
Frank Santiesteban <franksantiesteban@Franks-MacBook-Pro.local>
2018-05-23 21:03:22 -0400

    made modifications to js functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+
 var wins = 0;
 var losses = 0;
 var guesses = 9;
@@ -9,42 +10,47 @@ var letterToGuess = null;
 
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-
 var updateGuessesLeft = function() {
-  // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
   document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
 };
 
-var updatelettertoguess = function() {
-    this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+var updateLetterToGuess = function() {
+  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
+var updateGuessesSoFar = function() {
+  document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
 
 };
+var reset = function() {
+  totalGuesses = 9;
+  guessesLeft = 9;
+  guessedLetters = [];
 
-var updateGuessessofar = function () {
-    document.querySelector('#let').innerHTML = "Your guesses so far:" + guessedLetters.join(', ');
-
-};
-
-var reset = function () {
-    totalGuesses = 9;
-    guessesLeft = 9;
-    guessedLetters = [];
-
-    updateLettertoGuess();
-    updateGuessesLeft();
-    updateGuessessofar();
-    
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();  
 }
-
 
 document.onkeyup = function(event) {
+    guessesLeft--;
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
+  guessedLetters.push(userGuess);
+  updateGuessesLeft();
+  updateGuessesSoFar();
 
-
-var userGuess = event.key;
-
-
-
-}
-
-
+  if (guessesLeft > 0){
+  if (userGuess == letterToGuess){
+     wins++;
+ document.querySelector('#wins').innerHTML = "Wins: " + wins;
+              
+    reset();
+            }
+        }else if(guessesLeft == 0){
+          
+            losses++;
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+          
+            reset();
+        }
+};
